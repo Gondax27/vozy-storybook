eaf185d97400d99d6bf913b8bfc059267fcff9a1
diegoanazco79 <djanazcob@gmail.com>
2023-02-02 16:28:37 -0500

    :books: DOCUMENTATION :: Adding docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,14 @@ module.exports = {
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
+    {
+      name: 'storybook-addon-sass-postcss',
+      options: {
+        sassLoaderOptions: {
+          implementation: require('sass'),
+        }
+      }
+    },
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
