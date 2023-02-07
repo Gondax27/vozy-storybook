aa50a105a8e348976c7c2eb75be55c889b7e745b
diegoanazco79 <djanazcob@gmail.com>
2023-02-07 11:10:02 -0500

    :sparkles: NEW FEATURE :: Adding a Radiobutton component

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -10,25 +10,55 @@ interface SwitchProps {
    */
   id?: string
   /**
-   * Common bootstrap styles
+   * Common bootstrap classes
    */
   className?: string
+  /**
+   * Common bootstrap classes
+   */
   labelClassName?: string
+  /**
+   * Common bootstrap styles
+   */
   labelLeftClassName?: string
+  /**
+   * Common bootstrap styles
+   */
   labelRightClassName?: string
+  /**
+   * Label to display on the left side of the switch
+   */
   leftLabel?: string
+  /**
+   * Label to display on the right side of the switch
+   */
   rightLabel?: string
+  /**
+   * Controls the active/inactive state of the switch
+   */
   active: boolean
+  /**
+   * Handles the disabled state of the switch
+   */
   disabled?: boolean
+  /**
+   * Change the size of the switch
+   */
   size?: 'lg' | 'xl' | undefined
+  /**
+   * Handles the function that will change the state of the switch
+   */
   onChange: (status: boolean) => void
 }
 
+/**
+ * This component rendering a input switch with a right/left label.
+*/
 export const VzSwitch = ({
   id,
-  className,
-  labelClassName,
-  leftLabel, labelLeftClassName, rightLabel, labelRightClassName,
+  className = '',
+  labelClassName = '',
+  leftLabel, labelLeftClassName = '', rightLabel, labelRightClassName = '',
   active,
   disabled,
   size,
